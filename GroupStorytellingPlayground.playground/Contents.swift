a092ed34f8cdc09cf54980407fab90ec6f40811b
James Larcombe <jameslarcombe@gmail.com>
2015-08-10 17:02:32 -0400

    Commit second sentence

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -3,3 +3,5 @@
 import UIKit
 
 var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
+
+var secondStorySentence = "There was a large android beast at the door and our fearless leader was afraid."
